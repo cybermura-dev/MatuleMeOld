905041ece2292e570fb36726500bfe28948c40f4
Takeshiko <takeshikodev@gmail.com>
2025-03-14 00:42:14 +0100

    Update RegisterViewModel.kt

--- app/src/main/java/ru/takeshiko/matuleme/presentation/register/RegisterViewModel.kt
+++ app/src/main/java/ru/takeshiko/matuleme/presentation/register/RegisterViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import io.github.jan.supabase.auth.user.UserInfo
 import kotlinx.coroutines.launch
 import ru.takeshiko.matuleme.R
 import ru.takeshiko.matuleme.data.local.AppPreferencesManager
@@ -16,8 +17,8 @@ class RegisterViewModel(
     supabaseClientManager: SupabaseClientManager
 ) : ViewModel() {
 
-    private val _registrationResult = MutableLiveData<AuthResult<Any>>()
-    val registrationResult: LiveData<AuthResult<Any>> = _registrationResult
+    private val _registrationResult = MutableLiveData<AuthResult<UserInfo?>>()
+    val registrationResult: LiveData<AuthResult<UserInfo?>> = _registrationResult
 
     private val authRepository = AuthRepositoryImpl(supabaseClientManager)
 
@@ -39,8 +40,8 @@ class RegisterViewModel(
             }
 
             when (val result = authRepository.register(email, password)) {
-                is AuthResult.Success -> _registrationResult.postValue(result)
-                is AuthResult.Error -> _registrationResult.postValue(result)
+                is AuthResult.Success -> _registrationResult.value = AuthResult.Success(result.data)
+                is AuthResult.Error -> _registrationResult.value = AuthResult.Error(result.message)
             }
         }
     }
